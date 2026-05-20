7e683d9ff13a9363b26ab02244c4b668d3e89ebf
I1HN <brunaaaa2009@gmail.com>
2026-05-20 10:21:01 -0300

    termino do ex1

---
+++ script.js
@@ -0,0 +1,25 @@
+let numeros = [];
+
+for (let i = 0; i < 5; i++) {
+    
+     while (true) {
+       numeros[i] = prompt("Digite o número");
+
+        // trim remove os espaços em branco da variável
+        if (numeros[i].trim() == "") {
+            alert("Número inválido");
+        } else {
+            break; // Vai "brecar" o looping
+        }
+    }
+}
+
+// \n quebra a linha no alert!
+let mensagem = "=== NÚMEROS ===\n\n";
+
+for (let i = 0; i < numeros.length; i++) {
+    mensagem += "Número digitado: " + numeros[i] + "\n";
+    mensagem += "----------------\n";
+}
+
+alert(mensagem);
